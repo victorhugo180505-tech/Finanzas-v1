36ed3db5cda3f5c948d8ae5d4c3cd83a2726d715
Stevenz <victorhugo180505@gmail.com>
2026-04-21 08:20:15 -0600

    Inicio

---
+++ logica.py
@@ -0,0 +1,102 @@
+import itertools as iter
+
+class Movimiento :
+    def __init__(self,id,nombre,tipo,monto,fecha,estado="Activo",aceptaParcial = None,frecuencia = 0,penalizacionFija=False,porc_penalizacion = 0):
+        self.id = id
+        self.nombre = nombre
+        self.tipo = tipo
+        self.estado = estado
+        self.monto = monto
+        self.aceptaParcial = aceptaParcial
+        self.fecha = fecha
+        self.frecuencia = frecuencia
+        self.porc_penalizacion = porc_penalizacion
+        self.penalizacionFija = penalizacionFija
+    
+    def __str__(self):
+        cadena = f"id: {self.id} Nombre: {self.nombre} {self.tipo}-${self.monto} (Fecha: {self.fecha})"
+        return cadena
+    
+class Usuario:
+    def __init__(self,balance = 0, nombre = "",supervivencia=0):
+        self.balance = balance
+        self.nombre = nombre
+        self.supervivencia= supervivencia
+
+
+
+
+class SimulacionSemanal :
+    def __init__(self,movimientosIniciales = None):
+        
+        self.movimientos = []    
+        
+        for movimiento in movimientosIniciales:
+            self.movimientos.append(movimiento)
+
+    def agregarMovimiento(self,movement:Movimiento):
+        self.movimientos.append(movement)
+    
+
+    def eliminarMovimiento(self,id = -1, nombre = ""):
+        if id == -1:
+            for movimiento in self.movimientos:
+                if(movimiento.nombre==nombre):
+                    self.movimientos.remove(movimiento)
+                    break
+            
+            
+        else:
+            for movimiento in self.movimientos:
+                if(movimiento.id==id):
+                    self.movimientos.remove(movimiento)
+                    break
+
+
+    def correrSimulacion(self,user:Usuario):
+        """
+        Realiza cambios en el balance del usuario en base a los movimientos almacenados,
+        retorna una lista con los movimientos que no se alcanzaron a cubrir.
+        """
+        #egresos = []
+        eventos = []
+        deudas = []
+        for movimiento in self.movimientos:
+
+
+            """if movimiento.estado == "Activo":
+                eventos.append(movimiento)
+                if movimiento.tipo == "Ingreso":
+                    user.balance+=movimiento.monto
+                elif movimiento.tipo == "Deuda":
+                    egresos.append(movimiento)
+                else:
+                    user.balance-=movimiento.monto"""
+            if movimiento.estado == "Activo" and (movimiento.tipo == "Ingreso" or movimiento.tipo == "Gasto"):
+                eventos.append(movimiento)
+            else:
+                deudas.append(movimiento)
+        #aqui ya filtramos para quedarnos con los activos y descartar los que ya no me importan, tambien filtramos solo los obligatorios
+        #Modificando
+
+
+
+
+
+
+        
+
+
+
+
+       
+        #return balance
+            
+
+            
+
+
+
+    
+        
+    
---
+++ simuladorFinanciero.py
@@ -0,0 +1,28 @@
+import logica as l
+
+prueba =  l.Movimiento(id=1,nombre="IngresoPrueba",tipo="Ingreso",estado="Activo",monto=550,fecha="2026/04/16",aceptaParcial=None,frecuencia=0,porc_penalizacion=0)
+prueba2 =  l.Movimiento(id=2,nombre="GastoPrueba",tipo="Gasto",estado="Activo",monto=500,fecha="2026/04/17",aceptaParcial=False,frecuencia=0,porc_penalizacion=0)
+#print(prueba2)
+arreglo = []
+arreglo.append(prueba)
+arreglo.append(prueba2)
+simulacion1 = l.SimulacionSemanal(arreglo)
+
+prueba3 =  l.Movimiento(id=3,nombre="DeudaPrueba",tipo="Deuda",estado="Activo",monto=50,fecha="2026/04/17",aceptaParcial=True,frecuencia=0,porc_penalizacion=20)
+simulacion1.agregarMovimiento(prueba3)
+
+for movimiento in simulacion1.movimientos:
+    print(movimiento)
+
+simulacion1.eliminarMovimiento(nombre="DeudaPrueba")
+print("\n\n\n")
+for movimiento in simulacion1.movimientos:
+    print(movimiento)
+
+userTester = l.Usuario(nombre = "krazy",balance=128)
+
+print(userTester.balance)
+simulacion1.correrSimulacion(userTester)
+#userTester.balance = 
+#userTester.balance-=100
+print(userTester.balance)
